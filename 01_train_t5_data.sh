948e11eefba7a6f39b884c2a76014622edd92caa
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-04-05 15:29:54 +0700

    Update 01_train_t5_data.sh

--- 01_train_t5_data.sh
+++ 01_train_t5_data.sh
@@ -7,8 +7,8 @@
 
 CUDA_VISIBLE_DEVICES=0 python  run_gen.py  --do_train --do_eval --do_eval_bleu  \
         --task refine --sub_task small --model_type codet5 --data_num -1    \
-        --num_train_epochs  10  \
-        --warmup_steps 500 \
+        --num_train_epochs  8  \
+        --warmup_steps 400 \
         --learning_rate 5e-5 --patience 3 --beam_size 5\
         --gradient_accumulation_steps 1 \
         --tokenizer_name=Salesforce/codet5-base  \
@@ -19,4 +19,4 @@ CUDA_VISIBLE_DEVICES=0 python  run_gen.py  --do_train --do_eval --do_eval_bleu  
         --summary_dir ./model/code2review_t5_data_task2/summary/   --save_last_checkpoints --always_save_model \
         --res_dir ./model/code2review_t5_data_task2/outputs/results \
         --res_fn  ./model/code2review_t5_data_task2/outputs/results/summarize_codet5_base.txt  \
-        --train_batch_size 4 --eval_batch_size 4 --max_source_length 512 --max_target_length 100
+        --train_batch_size 8 --eval_batch_size 8 --max_source_length 512 --max_target_length 100
