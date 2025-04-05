ce14c0d966cbbed52a3b5eb9029bed6282b209bd
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-04-05 15:30:19 +0700

    Update 02_test_all.sh

--- 02_test_all.sh
+++ 02_test_all.sh
@@ -20,8 +20,8 @@
 mkdir ./model/code2review_t5_data_task2/outputs/results_beam5_hyp5/
 CUDA_VISIBLE_DEVICES=0 python  run_gen2.py  --do_test --do_eval_bleu  \
         --task refine --sub_task small --model_type codet5 --data_num -1    \
-        --num_train_epochs  10  \
-        --warmup_steps 500 \
+        --num_train_epochs  8  \
+        --warmup_steps 400 \
         --learning_rate 5e-5 --patience 3 --beam_size 5\
         --gradient_accumulation_steps 1 \
         --tokenizer_name=Salesforce/codet5-base  \
@@ -32,4 +32,4 @@ CUDA_VISIBLE_DEVICES=0 python  run_gen2.py  --do_test --do_eval_bleu  \
         --summary_dir ./model/code2review_t5_data_task2/summary/   --save_last_checkpoints --always_save_model \
         --res_dir ./model/code2review_t5_data_task2/outputs/results_beam5_hyp5/ \
         --res_fn  ./model/code2review_t5_data_task2/outputs/results_beam5_hyp5/summarize_codet5_base.txt  \
-        --train_batch_size 4 --eval_batch_size 4 --max_source_length 512 --max_target_length 100
+        --train_batch_size 8 --eval_batch_size 8 --max_source_length 512 --max_target_length 100
