a0049137edc858b71c577e9c6686c551e0a45d58
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-04-09 09:36:40 +0700

    Update run_gen.py

--- run_gen.py
+++ run_gen.py
@@ -279,9 +279,19 @@ def main():
                 if args.data_num == -1:
                     tb_writer.add_scalar('dev_ppl', eval_ppl, cur_epoch)
 
+                if args.save_last_checkpoints:
+                    last_output_dir = os.path.join(args.output_dir, 'checkpoint-last')
+                    if not os.path.exists(last_output_dir):
+                        os.makedirs(last_output_dir)
+                    model_to_save = model.module if hasattr(model, 'module') else model
+                    output_model_file = os.path.join(last_output_dir, "pytorch_model.bin")
+                    torch.save(model_to_save.state_dict(), output_model_file)
+                    logger.info("Save the last model into %s", output_model_file)
+
                 if eval_ppl < best_ppl:
                     not_loss_dec_cnt = 0
                     logger.info("  Best ppl:%s", eval_ppl)
+                    logger.info("  " + "*" * 20)
                     fa.write("[%d] Best ppl changed into %.4f\n" % (cur_epoch, eval_ppl))
                     best_ppl = eval_ppl
                     output_dir = os.path.join(args.output_dir, 'checkpoint-best-ppl')
@@ -290,4 +300,94 @@ def main():
                     if args.always_save_model:
                         model_to_save = model.module if hasattr(model, 'module') else model
                         output_model_file = os.path.join(output_dir, "pytorch_model.bin")
-                        torch.save(model_to_sav)
+                        torch.save(model_to_save.state_dict(), output_model_file)
+                        logger.info("Save the best ppl model into %s", output_model_file)
+                else:
+                    not_loss_dec_cnt += 1
+                    logger.info("Ppl does not decrease for %d epochs", not_loss_dec_cnt)
+                    if all([x > args.patience for x in [not_bleu_em_inc_cnt, not_loss_dec_cnt]]):
+                        early_stop_str = "[%d] Early stop as not_bleu_em_inc_cnt=%d, and not_loss_dec_cnt=%d\n" % (
+                            cur_epoch, not_bleu_em_inc_cnt, not_loss_dec_cnt)
+                        logger.info(early_stop_str)
+                        fa.write(early_stop_str)
+                        break
+
+                logger.info("***** CUDA.empty_cache() *****")
+                torch.cuda.empty_cache()
+
+                if args.do_eval_bleu:
+                    eval_examples, eval_data = load_and_cache_gen_data(args, args.dev_filename, pool, tokenizer, 'dev',
+                                                                       only_src=True, is_sample=True)
+                    result = eval_bleu_epoch(args, eval_data, eval_examples, model, tokenizer, 'dev', 'e%d' % cur_epoch)
+                    dev_bleu, dev_em = result['bleu'], result['em']
+                    if args.task in ['summarize']:
+                        dev_bleu_em = dev_bleu
+                    elif args.task in ['defect']:
+                        dev_bleu_em = dev_em
+                    else:
+                        dev_bleu_em = dev_bleu + dev_em
+                    if args.data_num == -1:
+                        tb_writer.add_scalar('dev_bleu_em', dev_bleu_em, cur_epoch)
+
+                    if dev_bleu_em > best_bleu_em:
+                        not_bleu_em_inc_cnt = 0
+                        logger.info("  [%d] Best bleu+em: %.2f (bleu: %.2f, em: %.2f)",
+                                    cur_epoch, dev_bleu_em, dev_bleu, dev_em)
+                        logger.info("  " + "*" * 20)
+                        best_bleu_em = dev_bleu_em
+                        fa.write("[%d] Best bleu+em changed into %.2f (bleu: %.2f, em: %.2f)\n" % (
+                            cur_epoch, best_bleu_em, dev_bleu, dev_em))
+                        output_dir = os.path.join(args.output_dir, 'checkpoint-best-bleu')
+                        if not os.path.exists(output_dir):
+                            os.makedirs(output_dir)
+                        if args.data_num == -1 or args.always_save_model:
+                            model_to_save = model.module if hasattr(model, 'module') else model
+                            output_model_file = os.path.join(output_dir, "pytorch_model.bin")
+                            torch.save(model_to_save.state_dict(), output_model_file)
+                            logger.info("Save the best bleu model into %s", output_model_file)
+                    else:
+                        not_bleu_em_inc_cnt += 1
+                        logger.info("Bleu does not increase for %d epochs", not_bleu_em_inc_cnt)
+                        fa.write(
+                            "[%d] Best bleu+em (%.2f) does not drop changed for %d epochs, cur bleu+em: %.2f (bleu: %.2f, em: %.2f)\n" % (
+                                cur_epoch, best_bleu_em, not_bleu_em_inc_cnt, dev_bleu_em, dev_bleu, dev_em))
+                        if all([x > args.patience for x in [not_bleu_em_inc_cnt, not_loss_dec_cnt]]):
+                            stop_early_str = "[%d] Early stop as not_bleu_em_inc_cnt=%d, and not_loss_dec_cnt=%d\n" % (
+                                cur_epoch, not_bleu_em_inc_cnt, not_loss_dec_cnt)
+                            logger.info(stop_early_str)
+                            fa.write(stop_early_str)
+                            break
+
+            logger.info("***** CUDA.empty_cache() *****")
+            torch.cuda.empty_cache()
+
+        if args.local_rank in [-1, 0] and args.data_num == -1:
+            tb_writer.close()
+        logger.info("Finish training and take %s", get_elapse_time(t0))
+
+    if args.do_test:
+        logger.info("  " + "***** Testing *****")
+        logger.info("  Batch size = %d", args.eval_batch_size)
+        for criteria in ['best-bleu']:
+            file = os.path.join(args.output_dir, 'checkpoint-{}/pytorch_model.bin'.format(criteria))
+            logger.info("Reload model from {}".format(file))
+            model.load_state_dict(torch.load(file))
+            eval_examples, eval_data = load_and_cache_gen_data(args, args.test_filename, pool, tokenizer, 'test',
+                                                               only_src=True, is_sample=False)
+            result = eval_bleu_epoch(args, eval_data, eval_examples, model, tokenizer, 'test', criteria)
+            test_bleu, test_em = result['bleu'], result['em']
+            test_codebleu = result['codebleu'] if 'codebleu' in result else 0
+            result_str = "[%s] bleu-4: %.2f, em: %.4f, codebleu: %.4f\n" % (criteria, test_bleu, test_em, test_codebleu)
+            logger.info(result_str)
+            fa.write(result_str)
+            if args.res_fn:
+                with open(args.res_fn, 'a+') as f:
+                    f.write('[Time: {}] {}\n'.format(get_elapse_time(t0), file))
+                    f.write(result_str)
+
+    logger.info("Finish and take {}".format(get_elapse_time(t0)))
+    fa.write("Finish and take {}".format(get_elapse_time(t0)))
+    fa.close()
+
+if __name__ == "__main__":
+    main()
