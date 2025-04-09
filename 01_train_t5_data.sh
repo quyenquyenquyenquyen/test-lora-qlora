b512faff347d8017607ddf6267e33bae6a141676
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-04-09 09:00:28 +0700

    Update 01_train_t5_data.sh

--- 01_train_t5_data.sh
+++ 01_train_t5_data.sh
@@ -11,13 +11,13 @@ mkdir -p ./model/code2review_t5_data_task2/outputs/results
 # Chạy script huấn luyện CodeT5
 CUDA_VISIBLE_DEVICES=0 python run_gen.py  --do_train --do_eval --do_eval_bleu  \
         --task refine --sub_task small --model_type codet5 --data_num -1    \
-        --num_train_epochs  20  \
+        --num_train_epochs  3  \
         --warmup_steps 500 \
         --learning_rate 5e-5 --patience 3 --beam_size 5 \
         --gradient_accumulation_steps 1 \
         --tokenizer_name=Salesforce/codet5-base  \
         --model_name_or_path=Salesforce/codet5-base \
-        --data_dir "/kaggle/input/daataa1/task2_data/t5_data/codet5_format_data" \
+        --data_dir "/kaggle/input/daataa/task2_data/t5_data/codet5_format_data" \
         --cache_path ./model/code2review_t5_data_task2/cache/  \
         --output_dir ./model/code2review_t5_data_task2/outputs/  \
         --summary_dir ./model/code2review_t5_data_task2/summary/   --save_last_checkpoints --always_save_model \
