e4646180965a7db08d6bff18664173dbf63a9915
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-04-22 15:09:35 +0700

    Update models.py

--- models.py
+++ models.py
@@ -25,6 +25,7 @@ def build_or_load_gen_model(args):
     config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
     config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name)
+    
     if args.model_type == 'roberta':
         encoder = model_class.from_pretrained(args.model_name_or_path, config=config)
         decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
@@ -48,7 +49,7 @@ def build_or_load_gen_model(args):
 
 class RobertaClassificationHead(nn.Module):
     """Head for sentence-level classification tasks."""
-
+    
     def __init__(self, config):
         super().__init__()
         self.dense = Linear(config.hidden_size * 2, config.hidden_size)
@@ -71,7 +72,7 @@ class CloneModel(nn.Module):
         self.classifier = RobertaClassificationHead(config)
         self.args = args
 
-    def get_t5_vec(self, source_ids):
+    def get_model_vec(self, source_ids):
         attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
         outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
                                labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
@@ -80,40 +81,18 @@ class CloneModel(nn.Module):
 
         if len(torch.unique(eos_mask.sum(1))) > 1:
             raise ValueError("All examples must have the same number of <eos> tokens.")
+        
         vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
                                               hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_bart_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
-                                              hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_roberta_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        vec = self.encoder(input_ids=source_ids, attention_mask=attention_mask)[0][:, 0, :]
         return vec
 
     def forward(self, source_ids=None, labels=None):
         source_ids = source_ids.view(-1, self.args.max_source_length)
 
-        if self.args.model_type == 'codet5':
-            vec = self.get_t5_vec(source_ids)
-        elif self.args.model_type == 'bart':
-            vec = self.get_bart_vec(source_ids)
-        elif self.args.model_type == 'roberta':
-            vec = self.get_roberta_vec(source_ids)
+        vec = self.get_model_vec(source_ids)
 
         logits = self.classifier(vec)
-        prob = nn.functional.softmax(logits)
+        prob = nn.functional.softmax(logits, dim=-1)
 
         if labels is not None:
             loss_fct = nn.CrossEntropyLoss()
@@ -132,7 +111,7 @@ class DefectModel(nn.Module):
         self.classifier = Linear(config.hidden_size, 2)
         self.args = args
 
-    def get_t5_vec(self, source_ids):
+    def get_model_vec(self, source_ids):
         attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
         outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
                                labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
@@ -141,40 +120,18 @@ class DefectModel(nn.Module):
 
         if len(torch.unique(eos_mask.sum(1))) > 1:
             raise ValueError("All examples must have the same number of <eos> tokens.")
+        
         vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
                                               hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_bart_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
-                                              hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_roberta_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        vec = self.encoder(input_ids=source_ids, attention_mask=attention_mask)[0][:, 0, :]
         return vec
 
     def forward(self, source_ids=None, labels=None):
         source_ids = source_ids.view(-1, self.args.max_source_length)
 
-        if self.args.model_type == 'codet5':
-            vec = self.get_t5_vec(source_ids)
-        elif self.args.model_type == 'bart':
-            vec = self.get_bart_vec(source_ids)
-        elif self.args.model_type == 'roberta':
-            vec = self.get_roberta_vec(source_ids)
+        vec = self.get_model_vec(source_ids)
 
         logits = self.classifier(vec)
-        prob = nn.functional.softmax(logits)
+        prob = nn.functional.softmax(logits, dim=-1)
 
         if labels is not None:
             loss_fct = nn.CrossEntropyLoss()
@@ -216,20 +173,11 @@ class Seq2Seq(nn.Module):
         self.sos_id = sos_id
         self.eos_id = eos_id
 
-    def _tie_or_clone_weights(self, first_module, second_module):
-        """ Tie or clone module weights depending of weither we are using TorchScript or not
-        """
-        if self.config.torchscript:
-            first_module.weight = nn.Parameter(second_module.weight.clone())
-        else:
-            first_module.weight = second_module.weight
-
     def tie_weights(self):
         """ Make sure we are sharing the input and output embeddings.
             Export to TorchScript can't handle parameter sharing so we are cloning them instead.
         """
-        self._tie_or_clone_weights(self.lm_head,
-                                   self.encoder.embeddings.word_embeddings)
+        self._tie_or_clone_weights(self.lm_head, self.encoder.embeddings.word_embeddings)
 
     def forward(self, source_ids=None, source_mask=None, target_ids=None, target_mask=None, args=None):
         outputs = self.encoder(source_ids, attention_mask=source_mask)
@@ -239,22 +187,17 @@ class Seq2Seq(nn.Module):
             tgt_embeddings = self.encoder.embeddings(target_ids).permute([1, 0, 2]).contiguous()
             out = self.decoder(tgt_embeddings, encoder_output, tgt_mask=attn_mask,
                                memory_key_padding_mask=~source_mask)
-            # memory_key_padding_mask=(1 - source_mask).bool())
             hidden_states = torch.tanh(self.dense(out)).permute([1, 0, 2]).contiguous()
             lm_logits = self.lm_head(hidden_states)
-            # Shift so that tokens < n predict n
             active_loss = target_mask[..., 1:].ne(0).view(-1) == 1
             shift_logits = lm_logits[..., :-1, :].contiguous()
             shift_labels = target_ids[..., 1:].contiguous()
-            # Flatten the tokens
             loss_fct = nn.CrossEntropyLoss(ignore_index=-1)
             loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1))[active_loss],
                             shift_labels.view(-1)[active_loss])
-
             outputs = loss, loss * active_loss.sum(), active_loss.sum()
             return outputs
         else:
-            # Predict
             preds = []
             zero = torch.cuda.LongTensor(1).fill_(0)
             for i in range(source_ids.shape[0]):
@@ -271,7 +214,6 @@ class Seq2Seq(nn.Module):
                     tgt_embeddings = self.encoder.embeddings(input_ids).permute([1, 0, 2]).contiguous()
                     out = self.decoder(tgt_embeddings, context, tgt_mask=attn_mask,
                                        memory_key_padding_mask=~context_mask)
-                    # memory_key_padding_mask=(1 - context_mask).bool())
                     out = torch.tanh(self.dense(out))
                     hidden_states = out.permute([1, 0, 2]).contiguous()[:, -1, :]
                     out = self.lsm(self.lm_head(hidden_states)).data
@@ -286,116 +228,3 @@ class Seq2Seq(nn.Module):
 
             preds = torch.cat(preds, 0)
             return preds
-
-
-class Beam(object):
-    def __init__(self, size, sos, eos):
-        self.size = size
-        self.tt = torch.cuda
-        # The score for each translation on the beam.
-        self.scores = self.tt.FloatTensor(size).zero_()
-        # The backpointers at each time-step.
-        self.prevKs = []
-        # The outputs at each time-step.
-        self.nextYs = [self.tt.LongTensor(size)
-                           .fill_(0)]
-        self.nextYs[0][0] = sos
-        # Has EOS topped the beam yet.
-        self._eos = eos
-        self.eosTop = False
-        # Time and k pair for finished.
-        self.finished = []
-
-    def getCurrentState(self):
-        "Get the outputs for the current timestep."
-        batch = self.tt.LongTensor(self.nextYs[-1]).view(-1, 1)
-        return batch
-
-    def getCurrentOrigin(self):
-        "Get the backpointers for the current timestep."
-        return self.prevKs[-1]
-
-    def advance(self, wordLk):
-        """
-        Given prob over words for every last beam `wordLk` and attention
-        `attnOut`: Compute and update the beam search.
-
-        Parameters:
-
-        * `wordLk`- probs of advancing from the last step (K x words)
-        * `attnOut`- attention at the last step
-
-        Returns: True if beam search is complete.
-        """
-        numWords = wordLk.size(1)
-
-        # Sum the previous scores.
-        if len(self.prevKs) > 0:
-            beamLk = wordLk + self.scores.unsqueeze(1).expand_as(wordLk)
-
-            # Don't let EOS have children.
-            for i in range(self.nextYs[-1].size(0)):
-                if self.nextYs[-1][i] == self._eos:
-                    beamLk[i] = -1e20
-        else:
-            beamLk = wordLk[0]
-        flatBeamLk = beamLk.view(-1)
-        bestScores, bestScoresId = flatBeamLk.topk(self.size, 0, True, True)
-
-        self.scores = bestScores
-
-        # bestScoresId is flattened beam x word array, so calculate which
-        # word and beam each score came from
-        prevK = bestScoresId // numWords
-        self.prevKs.append(prevK)
-        self.nextYs.append((bestScoresId - prevK * numWords))
-
-        for i in range(self.nextYs[-1].size(0)):
-            if self.nextYs[-1][i] == self._eos:
-                s = self.scores[i]
-                self.finished.append((s, len(self.nextYs) - 1, i))
-
-        # End condition is when top-of-beam is EOS and no global score.
-        if self.nextYs[-1][0] == self._eos:
-            self.eosTop = True
-
-    def done(self):
-        return self.eosTop and len(self.finished) >= self.size
-
-    def getFinal(self):
-        if len(self.finished) == 0:
-            self.finished.append((self.scores[0], len(self.nextYs) - 1, 0))
-        self.finished.sort(key=lambda a: -a[0])
-        if len(self.finished) != self.size:
-            unfinished = []
-            for i in range(self.nextYs[-1].size(0)):
-                if self.nextYs[-1][i] != self._eos:
-                    s = self.scores[i]
-                    unfinished.append((s, len(self.nextYs) - 1, i))
-            unfinished.sort(key=lambda a: -a[0])
-            self.finished += unfinished[:self.size - len(self.finished)]
-        return self.finished[:self.size]
-
-    def getHyp(self, beam_res):
-        """
-        Walk back to construct the full hypothesis.
-        """
-        hyps = []
-        for _, timestep, k in beam_res:
-            hyp = []
-            for j in range(len(self.prevKs[:timestep]) - 1, -1, -1):
-                hyp.append(self.nextYs[j + 1][k])
-                k = self.prevKs[j][k]
-            hyps.append(hyp[::-1])
-        return hyps
-
-    def buildTargetTokens(self, preds):
-        sentence = []
-        for pred in preds:
-            tokens = []
-            for tok in pred:
-                if tok == self._eos:
-                    break
-                tokens.append(tok)
-            sentence.append(tokens)
-        return sentence
